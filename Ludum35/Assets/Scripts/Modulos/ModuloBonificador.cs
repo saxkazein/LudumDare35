a738f73637db80c11367d0dae39f4e8a1742bad2
Nach <ignacio.cabado@gmail.com>
2016-04-16 20:30:14 +0200

    Subida inicial de módulo de bonificaciones

---
+++ Ludum35/Assets/Scripts/Modulos/ModuloBonificador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+/**
+ * 
+ * Módulo que calcula las bonificaciones correspondientes
+ * 
+ */ 
+public class ModuloBonificador {
+
+
+	/**
+	 * Método que realiza la inicialización del módulo
+	 */ 
+	public void Start (DatosConfiguracion datosConfig) {
+	
+	}
+
+
+	/**
+	 * Módulo que realiza los cálculos necesarios para las bonificaciones
+	 */ 
+	public void calcularTurno(DatosTurno datosTurno){
+	}
+
+}
