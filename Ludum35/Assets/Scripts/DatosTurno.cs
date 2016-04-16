bd8731654fdd6f314f9ee0865d614806bbc460e6
Juan Carlos Liébana <jcliebana@outlook.es>
2016-04-16 16:54:10 +0200

    Creación datos turno.

---
+++ Ludum35/Assets/Scripts/DatosTurno.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Información básica de cada turno
+public class DatosTurno
+{
+
+    
+    public int nivelMejoraRoboticaInicial;
+    public int nivelMejoraAlimentoInicial;
+    public int nivelMejoraDefensaInicial;
+    public int numeroPoblacionInicial;
+    public int numeroRobotsOrdenPublico;
+    public int bonificadorConstruccionRobots;
+    public int bonificadorConstruccionPoblacion;
+    public int bonificadorAlimentos;
+    public int bonificadorResistenciaRobot;
+    public int bonificadorTorretas;
+    public int bonificadorInfiltracion;
+    public int numeroRecursosInicial;
+    public int numeroConstruccionElegida;
+    public int numeroRecursosInvertidosConstruccion;
+    public int numeroComidaConstruida;
+    public int numeroRobotsConstruidos;
+    public int nivelMejoraRoboticaResultante;
+    public int nivelMejoraAlimentoResultante;
+    public int nivelMejoraDefensaResultante;
+    public int numeroComidaInicial;
+    public int numeroComidaConsumida;
+    public int numeroMuertesPorHambre;
+    public bool flagExpedicionActiva;
+    public int turnosDuracionExpedicionActiva;
+    public int turnosRestantesExpedicion;
+    public int tipoEncuentro;
+    public int numeroRobotsInicio;
+    public int numeroRobotsExpedicion;
+    public int recursosRecuperadosExpedicion;
+    public int poblacionRecuperadaExpedicion;
+    public int numeroRobotsPerdidosExpedicion;
+    public int numeroCambiaformas;
+    public int numeroRobotsPerdidosInfiltracion;
+    public int numeroRecursosPerdidosInfiltracion;
+    public int numeroTurno;
+    public int numeroPoblacionPerdidaAtaque;
+    public int numeroPoblacionEncuentro;
+    public int numeroRobotsEncuentro;
+    public int numeroRecursosEncuentro;
+    public int numeroComidaEncuentro;
+
+}
--- Ludum35/Assets/Scripts/ModuloDefensa.cs
+++ Ludum35/Assets/Scripts/ModuloDefensa.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ModuloDefensa : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+public class ModuloDefensa {
+
+
+    void calculaTurno(DatosTurno datosTurno)
+    {
+        
+
+    }
+
 }
